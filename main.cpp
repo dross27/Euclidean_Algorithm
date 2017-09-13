47fc1fa3d8387c0d3567162d08a55ac8309ea4b5
Don Ross <dr415@outlook.com>
2017-09-13 00:38:06 -0400

    This commit has the foundation for the E.A Still needs to find a way to implement the previous remainder

--- main.cpp
+++ main.cpp
@@ -1,12 +1,44 @@
 #include<iostream>
 
-long int GCD(long int numA, long int numB)
-{
-
-
-}
+long int EuclideanAlgorithm(long int, long int);
+void checkForNegativeValues(long int &, long int &);
+void makeTheLargestNumFirst(long int &, long int &);
 
 int main()
 {
 
+}
+long int EuclideanAlgorithm(long int numA, long int numB)
+{
+    long int q, r, previousRemainder;
+
+    checkForNegativeValues(numA, numB);
+
+    makeTheLargestNumFirst(numA, numB);
+
+    do
+    {
+        q = numA / numB;
+        r = numA % numB;
+       
+
+    }while (r != 0);
+    
+
+}
+void checkForNegativeValues(long int &numA, long int &numB) 
+{
+    numA = numA > 0 ? numA : -numA;
+    numB = numB > 0 ? numB : -numB;
+}
+
+void makeTheLargestNumFirst(long int &numA, long int &numB)
+{
+    if (numB > numA)
+    {
+        long int temp;
+        temp = numA;
+        numA = numB;
+        numB = temp;
+    }
 }
